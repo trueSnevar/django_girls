1534ebbee21aa9ca6ca475f03d9004322187a3fb
aknyazev <aknyazev87@gmail.com>
2017-12-19 17:02:15 +0300

    Added urls for blog app, template for blog

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,5 +19,7 @@ from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = "home")
+    path('', views.home, name = "home"),
+    path('blog/', include('blog.urls'))
 ]
+	
