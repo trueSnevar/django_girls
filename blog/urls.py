4eb37a02b5f7c94421ca6801446554a147fb9dc2
aknyazev <aknyazev87@gmail.com>
2018-01-19 12:30:43 +0300

    Added view and detailed blog post as well

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    url('', views.post_list, name='post_list')
+   
+    url(r'^$', views.post_list, name='post_list'),
+    url('post/(?P<pk>\d+)/$', views.post_details, name='post_details'),
+  
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {"posts": posts})	
+	print("hule nada?")
+
+	return render(request, 'blog/post_list.html', {"posts": posts})
+
+def post_details(request, pk):
+	print("hule tobi nada?")
+	post = get_object_or_404(Post, pk=pk)
+	print("hule tobi nada?")
+	return render(request, 'blog/post_details.html', {'post': post})
